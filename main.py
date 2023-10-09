053bac2be7c2a2b5e363e3b12133104d62750554
Sarah Kublin <127888292+skub01@users.noreply.github.com>
2023-10-09 14:50:03 -0400

    progress bar setup

--- main.py
+++ main.py
@@ -12,14 +12,23 @@ ssl._DEFAULT_CERT_FILE = certifi.where()
 def startDownload():
     try: 
         ytLink = link.get()
-        ytObject = YouTube(ytLink)
+        ytObject = YouTube(ytLink, on_progress_callback=on_progress)
         video = ytObject.streams.get_highest_resolution()
         video.download()
+
+        title.configure(text=ytObject.title)
+        finishLabel.configure(text="")
         finishLabel.configure(text="Downloaded!")
     except RegexMatchError: 
         finishLabel.configure(text="Invalid YouTube link!")
     except Exception as e:
         finishLabel.configure(text=f"An error occurred: {str(e)}", text_color="red")
+
+def on_progress(stream, chunk, bytes_remaining):
+    total_size = stream.filesize
+    bytes_downloaded = total_size - bytes_remaining
+    percentage_of_completion = bytes_downloaded / total_size * 100
+    print(percentage_of_completion)
 
 # System settings
 customtkinter.set_appearance_mode("System")
@@ -42,6 +51,14 @@ link.pack()
 #Finished downloading
 finishLabel = customtkinter.CTkLabel(app, text="")
 finishLabel.pack()
+
+#Progress percent
+pPercentage = customtkinter.CTkLabel(app, text="0%")
+pPercentage.pack()
+
+progressBar = customtkinter.CTkProgressBar(app, width=400)
+progressBar.set(0.5)
+progressBar.pack(padx=10, pady=10)
 
 #Download button
 download = customtkinter.CTkButton(app, text="Download", command=startDownload)
