24aec5c06a866624394f7c81c78212a89f716932
Sarah Kublin <127888292+skub01@users.noreply.github.com>
2023-10-16 15:27:12 -0400

    progress bar appears only after download starts

--- main.py
+++ main.py
@@ -14,6 +14,8 @@ def startDownload():
         ytLink = link.get()
         ytObject = YouTube(ytLink, on_progress_callback=on_progress)
         video = ytObject.streams.get_highest_resolution()
+        progressBar.set(0.0)
+        progressBar.pack(padx=10, pady=10)
         video.download()
 
         title.configure(text=ytObject.title)
@@ -29,8 +31,7 @@ def on_progress(stream, chunk, bytes_remaining):
     bytes_downloaded = total_size - bytes_remaining
     percentage_of_completion = bytes_downloaded / total_size * 100
     per = str(int(percentage_of_completion))
-    pPercentage.configure(text=per + "%")
-    pPercentage.update()
+    
 
     progressBar.set(float(percentage_of_completion) / 100)
 
@@ -61,8 +62,6 @@ pPercentage = customtkinter.CTkLabel(app, text="0%")
 pPercentage.pack()
 
 progressBar = customtkinter.CTkProgressBar(app, width=400)
-progressBar.set(0.0)
-progressBar.pack(padx=10, pady=10)
 
 # Download button
 download = customtkinter.CTkButton(app, text="Download", command=startDownload)
